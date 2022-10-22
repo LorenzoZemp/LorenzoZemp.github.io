79c8b7fe74679331a618c7b50044b2cf14c8e634
Lorenzo <lorenzozemp@icloud.com>
2022-10-22 23:57:44 +1300

    Added placeholder greybox for Projects page

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
+// Placeholder
+const allProjNames = document.querySelectorAll(".project-name");
+const allProjTechs = document.querySelectorAll(".project-tech");
 
+allProjNames.forEach((element) => {
+  element.textContent = "Project Name";
+});
+
+allProjTechs.forEach((element) => {
+  element.textContent = "HTML CSS JS";
+});
