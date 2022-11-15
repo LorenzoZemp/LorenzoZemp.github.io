f7931d4f0c80e622e0b0def1075e08fb03f76b21
Lorenzo <lorenzozemp@icloud.com>
2022-11-15 13:54:41 +1300

    Added projects section

--- app.js
+++ app.js
@@ -2,9 +2,13 @@
 const allProjNames = document.querySelectorAll(".project-name");
 const allProjTechs = document.querySelectorAll(".project-tech");
 allProjNames.forEach((element) => {
-  element.textContent = "Project Name";
+  if (element.textContent === "") {
+    element.textContent = "Project Name";
+  }
 });
 
 allProjTechs.forEach((element) => {
-  element.textContent = "HTML CSS JS";
+  if (element.textContent === "") {
+    element.textContent = "HTML CSS JS";
+  }
 });
